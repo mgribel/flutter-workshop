96554476218d1c3eb5d43eb05784c6d8cbb59ce2
Marcos Gribel <gribel.marcos@gmail.com>
2019-07-13 01:06:30 +0200

    Step 1: Fix overflowed layout

--- lib/model/movies/detail/view/movie_detail_view.dart
+++ lib/model/movies/detail/view/movie_detail_view.dart
@@ -11,7 +11,6 @@ class MovieDetail extends StatefulWidget {
   State<StatefulWidget> createState() {
     return MovieDetailState();
   }
-
 }
 
 class MovieDetailState extends State<MovieDetail> {
@@ -27,62 +26,65 @@ class MovieDetailState extends State<MovieDetail> {
               pinned: true,
               backgroundColor: Theme.of(context).primaryColor,
               flexibleSpace: FlexibleSpaceBar(
-                  background: Image.network(widget.viewHandler.posterUrl,
-                    fit: BoxFit.cover,
-                  ))),
+                  background: Image.network(
+                widget.viewHandler.posterUrl,
+                fit: BoxFit.cover,
+              ))),
           SliverList(
             delegate: SliverChildListDelegate(
-              [Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(margin: EdgeInsets.only(top: 5.0)),
-                    Text(widget.viewHandler.title,
-                      style: TextStyle(
-                          fontSize: 25.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white
+              [
+                Container(
+                  padding: EdgeInsets.all(8),
+                  height: 200, //TODO(Step 1.1): Remove this line)
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[Container(
+                          padding: EdgeInsets.only(left: 70), //TODO(Step 1.2): Remove this line
+                          child: Text(
+                            widget.viewHandler.title,
+                            style: TextStyle(
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+
+                        ),
                       ),
-                    ),
-                    Container(margin: EdgeInsets.only(top: 8.0, bottom: 8.0)),
-                    Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.star,
-                          color: Colors.yellow,
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 16),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(horizontal: 8),
+                              child: Text(
+                                widget.viewHandler.voteAverage,
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.white),
+                              ),
+                            ),
+                            Expanded(
+                              child: Text(
+                                widget.viewHandler.releaseDate,
+                                textAlign: TextAlign.end,
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.white),
+                              ),
+                            ),
+                          ],
                         ),
-                        Container(
-                          margin: EdgeInsets.only(left: 1.0, right: 1.0),
+                      ),
+                      Container(
+                        child: Text(
+                          widget.viewHandler.description,
+                          style: TextStyle(fontSize: 16.0, color: Colors.white),
                         ),
-                        Text(
-                          widget.viewHandler.voteAverage,
-                          style: TextStyle(
-                              fontSize: 18.0,
-                              color: Colors.white
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(left: 10.0,
-                              right: 10.0),
-                        ),
-                        Text(
-                          widget.viewHandler.releaseDate,
-                          style: TextStyle(
-                              fontSize: 18.0,
-                              color: Colors.white
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(margin: EdgeInsets.only(top: 8.0,
-                        bottom: 8.0)),
-                    Text(widget.viewHandler.description, style: TextStyle(color: Colors.white),),
-                    Container(margin: EdgeInsets.only(top: 8.0,
-                        bottom: 8.0)),
-                  ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
               ],
             ),
           ),
